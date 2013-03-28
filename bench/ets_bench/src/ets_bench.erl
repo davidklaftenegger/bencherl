03397317975a94433aa5c452bcf2fd1737da8c7b
David Klaftenegger <david.klaftenegger@it.uu.se>
2013-03-28 18:07:15 +0100

    somewhat updated configuration for fixed benchmark lookup times are much higher

--- bench/ets_bench/src/ets_bench.erl
+++ bench/ets_bench/src/ets_bench.erl
@@ -11,12 +11,13 @@ bench_args(Version, _) ->
 	[KeyRange, InsDels, Reads, PartialResults] = case Version of
 		short -> [14, 15, 16, 1];
 		intermediate -> [18, 20, 22, 1];
-		long -> [24, 20, 26, 1]
+		long -> [22, 20, 24, 1]
 	end,
 	TableTypes = case Version of
 		short -> [set, ordered_set, {gi, null}];
 		intermediate -> [set, ordered_set ];
 		long -> [set, ordered_set, {gi, skiplist}, {gi, btreeset}, {gi, stlset}, {gi, stlhashset}]
+		%long -> [set, ordered_set]
 	end,
 	%% use deterministic seed for reproducable results
 	Seed = {0,0,0},
@@ -24,7 +25,7 @@ bench_args(Version, _) ->
 	%Seed = now(),
 	ConcurrencyOptions = [r], % options are: no, r, w, rw
 	[[TT,KeyRange,InsDels,Reads,C,PartialResults,Seed] || TT <- TableTypes, C <- ConcurrencyOptions ] ++ [[set,KeyRange,InsDels,Reads,rw,PartialResults,Seed], [ordered_set,KeyRange,InsDels,Reads,rw,PartialResults,Seed], [{gi, null},KeyRange,InsDels,Reads,rw,PartialResults,Seed]].
-%	[[TT,KeyRange,InsDels,Reads,C,PartialResults,Seed] || TT <- TableTypes, C <- ConcurrencyOptions ].
+	%[[TT,KeyRange,InsDels,Reads,C,PartialResults,Seed] || TT <- TableTypes, C <- ConcurrencyOptions ].
 
 run([Type, _K, _W, _R, C, _Parts, Seed], _, _) ->
 	% this is a setup
